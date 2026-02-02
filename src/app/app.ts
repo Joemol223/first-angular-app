2f70c322f3b6b6ac8a680e8bf9d3311ce8ef5f3b
Joemol Joy <joemoljoy.a@gmail.com>
2026-02-02 10:08:49 -0500

    feat: enhance app functionality with new input features and template handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first-angular-app');
   name = signal('Joe');
+  val = '';
+  ok = true;
+  items = ['A','B','C'];
+   type: 'info' | 'warning' | 'success' = 'info';
+  msg = 'Hello';
+  count = 0;
+  text='';
 }
